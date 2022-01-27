15e8a7c32f9cf289fa538c47f591ac04176d646d
kwan <sleepmanleung@gmail.com>
2022-01-27 14:20:23 -0800

    look.ts: change lootAtPlayer function signiture to support return success status
    add lookAtFront

--- src/modules/look.ts
+++ src/modules/look.ts
@@ -1,16 +1,17 @@
 // @ts-check
 import mineflayer from 'mineflayer'
 
-export const lookAtPlayer = async function(bot:mineflayer.Bot,playerName:string){
+export const lookAtPlayer = async function(bot:mineflayer.Bot,playerName:string):Promise<boolean>{
 
   const player = bot.players[playerName]
-  if(!player) return
+  if(!player) return false
   const playerEntity = player.entity
-  if(!playerEntity) return
+  if(!playerEntity) return false
 
   var pos = playerEntity.position.offset(0,playerEntity.height,0)
   
   await bot.lookAt(pos)
+  return true
 }
 
 export const lookAtMaster = async function(this:mineflayer.Bot){
@@ -22,4 +23,15 @@ export const lookAtMaster = async function(this:mineflayer.Bot){
   var pos = playerEntity.position.offset(0,playerEntity.height,0)
   
   await this.lookAt(pos)
+  return
+}
+
+export const lookAtFront = async function(bot:mineflayer.Bot):Promise<Boolean>{
+  if(bot.entity){
+    var pos = bot.entity.position.offset(1,bot.entity.height,0)
+    await bot.lookAt(pos)
+    return true
+  } else {
+    return false
+  }
 }
