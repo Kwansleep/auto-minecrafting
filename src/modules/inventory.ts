53f51b693e01950703c954ffb3c95beb343392c9
kwan <sleepmanleung@gmail.com>
2022-01-27 14:24:51 -0800

    items.ts: move analyzeSlots to inventory.ts
    add function allowEatFor
    
    inventory.ts:
    refactor throwAll to accept addition playerName as player to look at when throwing
    
    fix throwAll to account for equipped slots

--- src/modules/inventory.ts
+++ src/modules/inventory.ts
@@ -3,19 +3,46 @@ import mineflayer from 'mineflayer'
 // local modules
 import { SETTINGS } from '../settings.js'
 import { chatIsWhisperFromMaster } from './chats.js'
-import { analyzeSlots } from './items.js'
 import { lookAtPlayer } from './look.js'
+import {Item} from 'prismarine-item'
 
-export const throwAll = async function(bot:mineflayer.Bot){
+export const throwAll = async function(bot:mineflayer.Bot,lookAt:string = SETTINGS.masterName){
 
-  await lookAtPlayer(bot,SETTINGS.masterName)
+  await lookAtPlayer(bot,lookAt)
 
-  // TODO: can check to recursive style and modularize this
+  // TODO: maybe move to an slot by slot approach?
   let botItemList = bot.inventory.slots
   let [ids, count] = analyzeSlots(botItemList)
+  let untossed:number[] = [] 
 
   for(let i = 0; i < ids.length; i++){
-    await bot.toss(ids[i],null,count[i])
+    try {
+      await bot.toss(ids[i],null,count[i])
+    } catch {
+      untossed.push(i)
+    }
+  }
+
+  if(untossed.length == 0){
+    return 
+  }
+
+  // try to unequip everything
+  try {
+    await bot.unequip('head')
+    await bot.unequip('torso')
+    await bot.unequip('legs')
+    await bot.unequip('feet')
+    await bot.unequip('off-hand') 
+  } catch {}
+  
+  for(let i = 0; i < untossed.length; i++){
+    let index = untossed[i]
+    try {
+      await bot.toss(ids[index],null,count[index])
+    } catch {
+      console.log("failed to toss item with id ["+ids[index]+"]")
+    }
   }
 }
 
@@ -32,4 +59,24 @@ export const reportOutofSpaceAndIdle = function(bot:mineflayer.Bot){
       }
     }
   })
+}
+
+export const analyzeSlots = function(slots:Item[]):[number[],number[]]{
+  let counts:number[] = []
+  let ids:number[] = []
+
+  for(let i = 0; i< slots.length;i++){
+    if(slots[i]){
+      let index = ids.indexOf(slots[i].type)
+      if(index >= 0){
+        // already evaluted similar item
+        counts[index] += slots[i].count
+      } else {
+        // new item
+        ids.push(slots[i].type)
+        counts.push(slots[i].count)
+      }
+    }
+  }
+  return [ids,counts]
 }
--- src/modules/items.ts
+++ src/modules/items.ts
@@ -4,6 +4,7 @@ import {Window} from 'prismarine-windows'
 import {Item} from 'prismarine-item'
 import {Entity} from 'prismarine-entity'
 import * as v from 'vec3'
+import { sleep } from './utils.js'
 const { goals } = mcPathsFinder
 
 
@@ -35,6 +36,7 @@ export const collectItems = async function(bot:mineflayer.Bot,firstPos:v.Vec3,se
       await bot.pathfinder.goto(new goals.GoalBlock(entityPos.x,entityPos.y,entityPos.z))
     } catch (error) {
       console.log("had goto error in collect Items")
+      return botInventory.emptySlotCount() > 2
     }
     
   }
@@ -43,24 +45,42 @@ export const collectItems = async function(bot:mineflayer.Bot,firstPos:v.Vec3,se
   return botInventory.emptySlotCount() > 2
 }
 
-export const analyzeSlots = function(slots:Item[]):[number[],number[]]{
-  let counts:number[] = []
-  let ids:number[] = []
+export const collectItemsWithinHeight = async function(bot:mineflayer.Bot,firstPos:v.Vec3,secondPos:v.Vec3, height:number, borderAllowance:number = 3) : Promise<boolean>{
+  let xMax = Math.max(firstPos.x,secondPos.x) + borderAllowance
+  let zMax = Math.max(firstPos.z,secondPos.z) + borderAllowance
+  let xMin = Math.min(firstPos.x,secondPos.x) - borderAllowance
+  let zMin = Math.min(firstPos.z,secondPos.z) - borderAllowance
+  let yMin = Math.min(firstPos.y,secondPos.y) - 1
 
-  for(let i = 0; i< slots.length;i++){
-    if(slots[i]){
-      let index = ids.indexOf(slots[i].type)
-      if(index >= 0){
-        // already evaluted similar item
-        counts[index] += slots[i].count
-      } else {
-        // new item
-        ids.push(slots[i].type)
-        counts.push(slots[i].count)
-      }
+  let hasItem:boolean = true
+  let botInventory = bot.inventory as unknown as Window
+  
+  while(hasItem && botInventory.emptySlotCount() > 2){
+    let nextEntity = bot.nearestEntity((entity:Entity)=>{
+      if(!entity){
+        return false
+      } 
+      let pos = entity.position
+  
+      return entity.type === 'object' && pos.x <= xMax && pos.x >= xMin && pos.z <= zMax && pos.z >= zMin && (pos.y <= height + yMin + 1)
+    })
+
+    if(!nextEntity){
+      hasItem = false
+      break;
     }
+    let entityPos = nextEntity.position
+    try {
+      await bot.pathfinder.goto(new goals.GoalBlock(entityPos.x,entityPos.y,entityPos.z))
+    } catch (error) {
+      console.log("had goto error in collect Items")
+      return false
+    }
+    
   }
-  return [ids,counts]
+  console.log("finish picking")
+
+  return botInventory.emptySlotCount() > 2
 }
 
 export const enableEating = function(bot:mineflayer.Bot){
@@ -79,7 +99,23 @@ export const enableEating = function(bot:mineflayer.Bot){
     //@ts-ignore
     else bot.autoEat.enable() 
   })
-
+}
+export const allowEatFor = async function(bot:mineflayer.Bot,interval:number = 10000){
+  //@ts-ignore
+  bot.autoEat.options.priority = "foodPoints"
+  //@ts-ignore
+  bot.autoEat.options.bannedFood = []
+  //@ts-ignore
+  bot.autoEat.options.startAt = 19
+  //@ts-ignore
+  bot.autoEat.options.eatingTimeout = 5
+  //@ts-ignore
+  bot.autoEat.enable()
+  //@ts-ignore
+  bot.autoEat.eat()
+  await sleep(interval)
+  //@ts-ignore 
+  bot.autoEat.disable()
 }
 
 export function itemToPrettyString (item:Item,num:number,itemArray:Item[]) {
